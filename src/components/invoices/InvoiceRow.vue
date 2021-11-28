f725cb97a4df5dd17f821ad6a6875029acc5abf4
Zeeshan Khan <zkhan1093@gmail.com>
2021-11-28 14:51:18 +0530

    Adding support for date columns in invoice

--- src/components/invoices/InvoiceRow.vue
+++ src/components/invoices/InvoiceRow.vue
@@ -1,13 +1,37 @@
 <template>
     <tr>
         <td v-for="col in columns" :key="index + col.id">
-          <AppEditable :value="row[col.id]"
-                        :errors="errors"
-                        :field="`rows.${index}.${col.id}`"
-                        :placeholder="$t(`enter_${col.id}`)"
-                        @change="updateProp(col, $event)"
-                        style="display: flex"
-                        />
+          <template v-if="col.type === Date">
+            <span class="editable__item" v-b-modal.modal_date>
+              <template v-if="row[col.id]">
+              {{ row[col.id] | date('DD MMM YYYY', 'YYYY-MM-DD') }}
+              </template>
+              <template v-else>
+                Select Date
+              </template>
+            </span>
+            <BModal id="modal_date"
+                    centered
+                    :title="$t(`enter_${col.id}`)"
+                    hide-footer
+                    size="sm"
+                    content-class="bg-base dp--24">
+                <AppDatePicker :value="row[col.id]"
+                              @change="updateProp(col, $event)"
+                              :errors="errors"
+                              :inline="true"
+                              :field="`rows.${index}.${col.id}`"/>
+            </BModal>
+          </template>
+          <template v-else>
+            <AppEditable :value="row[col.id]"
+                          :errors="errors"
+                          :field="`rows.${index}.${col.id}`"
+                          :placeholder="$t(`enter_${col.id}`)"
+                          @change="updateProp(col, $event)"
+                          style="display: flex"
+                          />
+          </template>
         </td>
         <template v-if="showTaxCols">
           <td v-for="(tax, taxIndex) in row.taxes" :title="tax.label" :key="`${index}_tax_${tax.label}`" >
@@ -33,8 +57,12 @@
 
 <script>
 import config from '@/config/app.config';
-import { formatCurrency } from '../../filters/currency.filter';
+import { BModal, VBModal } from 'bootstrap-vue';
+import { formatDate } from '@/filters/date.filter';
+import { formatCurrency } from '@/filters/currency.filter';
+import AppDatePicker from '@/components/form/AppDatePicker';
 import AppEditable from '../form/AppEditable';
+
 
 export default {
   props: ['row', 'errors', 'index'],
@@ -42,8 +70,14 @@ export default {
   i18nOptions: { namespaces: 'invoice-row' },
   components: {
     AppEditable,
+    AppDatePicker,
+    BModal,
+  },
+  directives: {
+    'b-modal': VBModal,
   },
   filters: {
+    date: formatDate,
     currency: formatCurrency,
   },
   computed: {
